abdc7c3388c102cdb52e9afa1913a34b38b8eccc
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-06-14 11:06:32 -0500

    Update spamWP.py

--- spamWP.py
+++ spamWP.py
@@ -1,4 +1,4 @@
-import pyautogui, time
+import pyautogui, time #import pyautogui with pip in cmd
 
 time.sleep(4) #time for running xd 
 numberOfMenssages = 50
@@ -7,5 +7,5 @@ j = 0
 
 for i in range(numeroDeMensajes):
     j = j + 1 #simple counter to make it more funny 
-    pyautogui.typewrite(f"Message Number: {contador}") #here goes the text wich we are going to send
+    pyautogui.typewrite(f"Message Number: {j}") #here goes the text wich we are going to send
     pyautogui.press("enter")
